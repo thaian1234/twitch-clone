7953bd3d15730051a175f0f95c5f0562a26f7736
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2024-01-08 20:54:00 +0700

    adding recommended list

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { Navbar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
+import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
 import { Container } from "./_components/container";
 
 export default function BrowseLayout({ children }: { children: ReactNode }) {
@@ -8,7 +8,9 @@ export default function BrowseLayout({ children }: { children: ReactNode }) {
 		<>
 			<Navbar />
 			<div className="flex h-full pt-20">
-				<Sidebar />
+				<Suspense fallback={<SidebarSkeleton />}>
+					<Sidebar />
+				</Suspense>
 				<Container>{children}</Container>
 			</div>
 		</>
