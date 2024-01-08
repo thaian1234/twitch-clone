ef6f4743081ae4037dc463a3dd85cc995e1cb78f
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2024-01-08 14:34:18 +0700

    database setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from "react";
+import { Logo } from "./_components/logo";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
 	return (
-		<div className="h-full flex items-center justify-center">
+		<div className="h-full flex flex-col items-center justify-center space-y-6">
+			<Logo />
 			{children}
 		</div>
 	);
